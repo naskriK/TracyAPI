27696f8c0657dadff7e21ce8433b32c1532d5070
naskriK <naskri.kamil@gmail.com>
2024-01-03 16:27:29 +0100

    feat: prisma
    Setup prisma migration, add lookup tasks fetching.

--- src/controllers/tour.controller.ts
+++ src/controllers/tour.controller.ts
@@ -1,7 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
-export const getAllTasks = (req: Request, res: Response) => {
+const prisma = new PrismaClient();
+
+export const getAllTasks = async (req: Request, res: Response) => {
+  const tasks = await prisma.task.findMany();
+
   return res.send({
     message: "Hello from tour controller!",
+    data: tasks,
   });
 };
